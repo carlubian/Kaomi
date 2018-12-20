e1d43b117fd6760141df52b521b2008e64346c6f
Carlos Lubián Fernández <carlubian@outlook.es>
2018-12-20 17:29:12 +0100

    Added example process

---
+++ HelloWorldProcess/OneTimeHelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using Kaomi.Core.Model;
+using System;
+
+namespace HelloWorldProcess
+{
+    public class OneTimeHelloWorld : OneTimeProcess
+    {
+        public override void OnFinalize() => throw new NotImplementedException();
+        public override void OnInitialize() => throw new NotImplementedException();
+        public override void OnIteration() => Console.WriteLine("Hello World from Kaomi process.");
+        public override void OnUserMessage(string message) => throw new NotImplementedException();
+    }
+}
